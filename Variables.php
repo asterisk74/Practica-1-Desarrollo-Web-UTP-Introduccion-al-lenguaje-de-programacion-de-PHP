45ef282bdc543ccf67e659a9d64e374ad2300a74
asterisk74 <andresdommar@gmail.com>
2026-09-04 10:02:49 -0500

    Variables.php

--- Variables.php
+++ Variables.php
@@ -1,6 +1,9 @@
 <?php
+//variable que guarda le nombre del autor 1
 $author1 = "John Doe";
+//variable que guarda le nombre del autor 2
 $author2 = "Max Mustermann";
+//Imprimir los nombre de los autores a la pantalla y un hola mundo
 echo "<h1>Hello World!</h1>
-<p> This dynamic web page was created by $author1 and $author2.</p>";
+<p> Esta pagina dinamica fue creada por $author1 y $author2.</p>";
 ?>
