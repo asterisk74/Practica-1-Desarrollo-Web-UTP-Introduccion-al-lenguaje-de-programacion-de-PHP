d1ccf854f478583a78e4462f146b16fc14555dc6
asterisk74 <andresdommar@gmail.com>
2026-09-04 10:00:20 -0500

    Lab1PulgadasACentimetros.php

--- Lab1PulgadasACentimetros.php
+++ Lab1PulgadasACentimetros.php
@@ -5,6 +5,8 @@
 </head>
 
 <body>
+    <h1>Convertidor de pulgadas a centimetros</h1>
+
     <form method="post" action="">
         Ingrese las pulgadas:
         <input type="number" name="pulgadas" step="any" required>
@@ -18,10 +20,19 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
     {
     $pulgadas = $_POST["pulgadas"];
+    if($pulgadas<0)
+        {
+            //respuesta al usuario si introuduce un numero negativo
+            echo "El numero a convertir no puede ser negativo";
+        }
+    else
+        {
+            //conversion de pulgadas a centimetros
+            $centimetros = $pulgadas * PULGADA_A_CENTIMETROS;
+            //resultado impreso a pantalla
+            echo "$pulgadas pulgadas en centimetros son $centimetros";
+        }
 
-    $centimetros = $pulgadas * PULGADA_A_CENTIMETROS;
-
-    echo "$pulgadas pulgadas en centimetros son $centimetros";
     }
 ?>
 </body>
