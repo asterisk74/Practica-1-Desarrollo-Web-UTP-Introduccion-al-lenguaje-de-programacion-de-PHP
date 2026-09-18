73453b289e2142dcc8cd9d52d1244edf718a3e73
asterisk74 <andresdommar@gmail.com>
2026-09-18 08:54:13 -0500

    Variables.php

--- Variables.php
+++ Variables.php
@@ -1,9 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+
+    <style>
+        body {
+            background-color: honeydew;
+            font-family: Arial;
+        }
+
+        h1 {
+            color: darkgreen;
+        }
+
+        p {
+            color: seagreen;
+            font-size: 18px;
+        }
+    </style>
+</head>
+
+<body>
+
 <?php
-//variable que guarda le nombre del autor 1
+//variable que guarda el nombre del autor 1
 $author1 = "John Doe";
-//variable que guarda le nombre del autor 2
+
+//variable que guarda el nombre del autor 2
 $author2 = "Max Mustermann";
-//Imprimir los nombre de los autores a la pantalla y un hola mundo
+
+//Imprimir los nombres de los autores a la pantalla y un hola mundo
 echo "<h1>Hello World!</h1>
-<p> Esta pagina dinamica fue creada por $author1 y $author2.</p>";
+<p>Esta pagina dinamica fue creada por $author1 y $author2.</p>";
 ?>
+
+</body>
+</html>
