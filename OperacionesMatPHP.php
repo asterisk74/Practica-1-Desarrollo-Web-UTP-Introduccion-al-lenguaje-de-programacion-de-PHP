72f0557618af0085244a8508605188b0f217b08c
asterisk74 <andresdommar@gmail.com>
2026-09-18 08:52:01 -0500

    OperacionesMatPHP.php

--- OperacionesMatPHP.php
+++ OperacionesMatPHP.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Operaciones Matemáticas</title>
+
+    <style>
+        body {
+            background-color: aliceblue;
+            font-family: Arial;
+        }
+
+        h1 {
+            color: navy;
+        }
+
+        h2 {
+            color: steelblue;
+        }
+
+        .resultado {
+            color: black;
+        }
+    </style>
+</head>
+
+<body>
+
 <?php
 
 echo "<h1>Operaciones Matemáticas</h1>";
@@ -9,7 +36,7 @@ $num = 4;
 $num2 = 7;
 $suma = $num + $num2;
 
-echo "La suma de $num y $num2 es: $suma <br><br>";
+echo "<p class='resultado'>La suma de $num y $num2 es: $suma</p>";
 
 //Resta
 echo "<h2>Resta</h2>";
@@ -18,7 +45,7 @@ $num = 10;
 $num2 = 3;
 $resta = $num - $num2;
 
-echo "La resta de $num y $num2 es: $resta <br><br>";
+echo "<p class='resultado'>La resta de $num y $num2 es: $resta</p>";
 
 //Multiplicacion
 echo "<h2>Multiplicacion</h2>";
@@ -27,7 +54,7 @@ $num = 5;
 $num2 = 6;
 $multiplicacion = $num * $num2;
 
-echo "La multiplicacion de $num y $num2 es: $multiplicacion <br><br>";
+echo "<p class='resultado'>La multiplicacion de $num y $num2 es: $multiplicacion</p>";
 
 //Division
 echo "<h2>Division</h2>";
@@ -36,7 +63,7 @@ $num = 20;
 $num2 = 4;
 $division = $num / $num2;
 
-echo "La division de $num y $num2 es: $division <br><br>";
+echo "<p class='resultado'>La division de $num y $num2 es: $division</p>";
 
 //Potenciacion
 echo "<h2>Potenciacion</h2>";
@@ -45,7 +72,7 @@ $base = 2;
 $exponente = 3;
 $potencia = $base ** $exponente;
 
-echo "La potencia de $base elevado a $exponente es: $potencia <br><br>";
+echo "<p class='resultado'>La potencia de $base elevado a $exponente es: $potencia</p>";
 
 //Modulo
 echo "<h2>Modulo Parte 1</h2>";
@@ -54,7 +81,7 @@ $num = 15;
 $num2 = 4;
 $modulo = $num % $num2;
 
-echo "El modulo de $num y $num2 es: $modulo <br><br>";
+echo "<p class='resultado'>El modulo de $num y $num2 es: $modulo</p>";
 
 //Redondeo
 echo "<h2>Redondeo</h2>";
@@ -65,9 +92,9 @@ $redondeado = round($num);
 $redondeado_arriba = ceil($num);
 $redondeado_abajo = floor($num);
 
-echo "El numero redondeado es: $redondeado <br><br>";
-echo "El numero redondeado hacia arriba es: $redondeado_arriba <br><br>";
-echo "El numero redondeado hacia abajo es: $redondeado_abajo <br><br>";
+echo "<p class='resultado'>El numero redondeado es: $redondeado</p>";
+echo "<p class='resultado'>El numero redondeado hacia arriba es: $redondeado_arriba</p>";
+echo "<p class='resultado'>El numero redondeado hacia abajo es: $redondeado_abajo</p>";
 
 //Modulo parte 2
 echo "<h2>Modulo Parte 2</h2>";
@@ -77,10 +104,10 @@ $modulo2 = (5 % -3);
 $modulo3 = (-5 % 3);
 $modulo4 = (-5 % -3);
 
-echo "El rsultado de (5 % 3) es: $modulo1 <br><br>";
-echo "El rsultado de (5 % -3) es: $modulo2 <br><br>";
-echo "El rsultado de (-5 % 3) es: $modulo3 <br><br>";
-echo "El rsultado de (-5 % -3) es: $modulo4 <br><br>";
+echo "<p class='resultado'>El resultado de (5 % 3) es: $modulo1</p>";
+echo "<p class='resultado'>El resultado de (5 % -3) es: $modulo2</p>";
+echo "<p class='resultado'>El resultado de (-5 % 3) es: $modulo3</p>";
+echo "<p class='resultado'>El resultado de (-5 % -3) es: $modulo4</p>";
 
 //Valor absoluto
 echo "<h2>Valor absoluto</h2>";
@@ -88,5 +115,9 @@ echo "<h2>Valor absoluto</h2>";
 $num = -7;
 $valor_absoluto = abs($num);
 
-echo "El valor absoluto de $num es: $valor_absoluto <br><br>";
+echo "<p class='resultado'>El valor absoluto de $num es: $valor_absoluto</p>";
+
 ?>
+
+</body>
+</html>
